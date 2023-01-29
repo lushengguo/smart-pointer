aaab8e3c46d6de00eb5bc06344d1c088682b8c36
Ubuntu <ubuntu@ip-172-31-35-121.ap-northeast-1.compute.internal>
2023-01-29 07:21:09 +0000

    finish unique_ptr

--- unique_ptr.h
+++ unique_ptr.h
@@ -1,11 +1,31 @@
 #pragma once
+#include <cstddef>
 #include <tuple>
 
 template <typename T>
 class default_deleter
 {
   public:
-    void operator()(T *t) { delete t; }
+    void operator()(T *t)
+    {
+        if (t)
+        {
+            delete t;
+        }
+    }
+};
+
+template <typename T>
+class default_deleter<T[]>
+{
+  public:
+    void operator()(T *t)
+    {
+        if (t)
+        {
+            delete[] t;
+        }
+    }
 };
 
 template <typename T, typename Deleter = default_deleter<T>>
@@ -13,21 +33,38 @@ class unique_ptr
 {
   public:
     using ThisType = unique_ptr<T, Deleter>;
-    
+    using CompressedPtr = std::tuple<T *, Deleter>;
+
   public:
-    unique_ptr();
+    unique_ptr() : t_(nullptr, default_deleter<T>()) {}
     unique_ptr(T *val) : t_(val, default_deleter<T>()) {}
+    unique_ptr(T *val, const Deleter &del) : t_(val, del) {}
     unique_ptr(const ThisType &) = delete;
-    unique_ptr(const ThisType &&rhs) : t_(rhs.t_)
-    {
-        rhs.t_ = std::tuple<T *, Deleter>();
-    }
+    unique_ptr(ThisType &&rhs) : t_(rhs.t_) { rhs.t_ = CompressedPtr(); }
 
     ~unique_ptr() { reset(); }
 
+    operator bool() const { return std::get<0>(t_); }
+    T &operator*() { return *get(); }
+    T *operator->() { return get(); }
+
     ThisType &operator=(const ThisType &rhs)
     {
+        reset();
         t_ = std::move(rhs.t_);
+        return *this;
+    }
+
+    ThisType &operator=(std::nullptr_t)
+    {
+        reset();
+        return *this;
+    }
+
+    ThisType &operator=(ThisType &&rhs)
+    {
+        reset();
+        std::get<0>(t_) = rhs.release();
         return *this;
     }
 
@@ -48,9 +85,20 @@ class unique_ptr
         if (ptr)
         {
             std::get<1>(t_)(ptr);
+            std::get<0>(t_) = nullptr;
         }
     }
 
+    void swap(ThisType &rhs) { std::swap(t_, rhs.t_); }
+
   private:
-    std::tuple<T *, Deleter> t_;
+    CompressedPtr t_;
 };
+
+template <typename T, typename... Args>
+unique_ptr<T> make_unique(Args... args)
+{
+    return unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+
+// T[] 的重载版就不写了
--- unique_ptr_test.cc
+++ unique_ptr_test.cc
@@ -1,7 +1,105 @@
 #include "unique_ptr.h"
 #include <gtest/gtest.h>
 
-TEST(unique_ptr, constructor)
+struct TestCtorDtor
+{
+    TestCtorDtor(bool &b) : b_(b) { b_ = true; }
+    ~TestCtorDtor() { b_ = false; }
+    bool &b_;
+};
+
+TEST(unique_ptr, construct)
+{
+    bool b = false;
+    {
+        unique_ptr<TestCtorDtor> u(nullptr);
+        EXPECT_FALSE(u);
+    }
+
+    {
+        unique_ptr<TestCtorDtor> u(new TestCtorDtor(b));
+        EXPECT_TRUE(b);
+        EXPECT_TRUE(u);
+    }
+
+    {
+        unique_ptr<TestCtorDtor> u(new TestCtorDtor(b));
+        unique_ptr<TestCtorDtor> u1(std::move(u));
+        EXPECT_TRUE(b);
+        EXPECT_TRUE(u1);
+        EXPECT_FALSE(u);
+    }
+}
+
+TEST(unique_ptr, destruct)
+{
+    bool b = false;
+    {
+        unique_ptr<TestCtorDtor> u(new TestCtorDtor(b));
+        EXPECT_TRUE(b);
+    }
+    EXPECT_FALSE(b);
+}
+
+TEST(unique_ptr, operatorAssign)
+{
+    bool b = false;
+
+    {
+        unique_ptr<TestCtorDtor> u(new TestCtorDtor(b));
+        EXPECT_TRUE(u);
+        unique_ptr<TestCtorDtor> u2 = std::move(u);
+        EXPECT_FALSE(u);
+        EXPECT_TRUE(u2);
+    }
+
+    {
+        unique_ptr<TestCtorDtor> u(new TestCtorDtor(b));
+        EXPECT_TRUE(u);
+        u = nullptr;
+        EXPECT_FALSE(u);
+    }
+}
+
+unique_ptr<int> pass_through(unique_ptr<int> u) { return u; }
+
+TEST(unique_ptr, move)
 {
     unique_ptr<int> u(new int);
+    EXPECT_TRUE(u);
+    unique_ptr<int> u2(pass_through(std::move(u)));
+    EXPECT_TRUE(u2);
+    EXPECT_FALSE(u);
+}
+
+void add(int *i)
+{
+    if (i)
+        *i += 1;
+}
+
+TEST(unique_ptr, diy_deleter)
+{
+    int i = 0;
+    {
+        unique_ptr<int, void (*)(int *)> u(&i, [](int *i) {
+            if (i)
+                *i = 1;
+        });
+        EXPECT_TRUE(i == 0);
+    }
+    EXPECT_TRUE(i == 1);
+
+    {
+        unique_ptr<int, decltype(&add)> u(&i, &add);
+        EXPECT_TRUE(i == 1);
+    }
+    EXPECT_TRUE(i == 2);
+}
+
+TEST(unique_ptr, make_unique)
+{
+    unique_ptr<int> u = make_unique<int>(1); 
+    EXPECT_TRUE(u);
+    EXPECT_TRUE((*u) == 1);
 }
